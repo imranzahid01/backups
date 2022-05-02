1871b1ccc65ca166114fee267dd2daf58be909a8
Imran Zahid <imranzahid@gmail.com>
2022-05-02 16:22:43 +0500

    Update HealthCheck URL

--- src/main/java/com/imranzahid/backup/util/HealthCheckUtil.java
+++ src/main/java/com/imranzahid/backup/util/HealthCheckUtil.java
@@ -4,38 +4,40 @@ import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.Closeable;
 import java.io.IOException;
 
 public class HealthCheckUtil implements Closeable {
   private static final Logger log = LoggerFactory.getLogger(HealthCheckUtil.class);
-  private static final String BASE_URL = "https://hc-ping.com/";
+  private static final String BASE_URL = System.getProperty("hc.url", "https://hc.btm-binds.com/ping/");
   private static final OkHttpClient httpClient = new OkHttpClient();
 
-  public static HealthCheckUtil getInstance(String uuid) {
+  @Nonnull public static HealthCheckUtil getInstance(@Nullable String uuid) {
     return new HealthCheckUtil(uuid);
   }
 
-  private final String uuid;
-  private HealthCheckUtil(String uuid) {
-    this.uuid = uuid;
+  @Nonnull private final String uuid;
+  private HealthCheckUtil(@Nullable String uuid) {
+    this.uuid = (uuid == null ? "" : uuid);
   }
 
-  private String getUrl() {
+  @Nonnull private String getUrl() {
     return BASE_URL + uuid;
   }
 
   public void start() {
-    if (uuid == null || uuid.isBlank()) {
+    if (uuid.isBlank()) {
       return;
     }
-    Request request = new Request.Builder()
-      .url(getUrl() + "/start").build();
-    execute(request);
+    execute(new Request.Builder()
+      .url(getUrl() + "/start")
+      .build());
   }
 
-  public void success(String message) {
-    if (uuid == null || uuid.isBlank()) {
+  public void success(@Nullable String message) {
+    if (uuid.isBlank()) {
       return;
     }
     Request.Builder request = new Request.Builder().url(getUrl());
@@ -45,8 +47,8 @@ public class HealthCheckUtil implements Closeable {
     execute(request.build());
   }
 
-  public void fail(String message) {
-    if (uuid == null || uuid.isBlank()) {
+  public void fail(@Nullable String message) {
+    if (uuid.isBlank()) {
       return;
     }
     Request.Builder request = new Request.Builder().url(getUrl() + "/fail");
@@ -56,17 +58,14 @@ public class HealthCheckUtil implements Closeable {
     execute(request.build());
   }
 
-  private void execute(Request request) {
+  private void execute(@Nonnull Request request) {
     try {
       try (Response response = httpClient.newCall(request).execute()) {
         if (!response.isSuccessful()) {
           log.error("Invalid response from httpCall : " + response);
         }
         ResponseBody body = response.body();
-        if (body != null) {
-          log.info(body.string());
-        }
-        else {
+        if (body == null) {
           log.error("Response body is null");
         }
       }
